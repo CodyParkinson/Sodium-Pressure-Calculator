f38b2dd4606d583d39410fa8c1587b7eff3e8c66
Cody Parkinson <codyparkinson22@gmail.com>
2024-02-13 17:40:14 -0700

    Updates

--- Main_Page.py
+++ Main_Page.py
@@ -1,5 +1,15 @@
 
 
+
+'''
+Imports
+'''
+
+from iapws import IAPWS97
+
+
+
+
 '''
 User Inputs
 '''
@@ -9,3 +19,15 @@ OuterCapsuleVoidVolume = 4.269
 
 # cm^3
 InnerCapsuleVoidVolume = 1.970
+
+
+
+
+'''
+Functions
+'''
+
+# Calculate water density from temp in C and pressure in MPa
+def water_density(temp_celsius, pressure_mpa):
+    water = IAPWS97(T=temp_celsius + 273.15, P=pressure_mpa)
+    return water.rho  # returns the density in kg/m^3
